8c206a950625a5e5f12f599d080da941d1820323
unknown <markivan23>
2023-11-23 16:31:17 +0800

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:resume2/personal.dart';
+import 'package:resume2/certificate.dart';
+import 'skill.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Curriculum Vitae",
+    home: homepage(),
+  ));
+}
+
+class homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 238, 237, 238),
+      appBar: AppBar(
+        title: const Center(
+          child: Text('My Curriculum Vitae'),
+        ),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Center(
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/sus.jpg'),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Mark Ivan S. Daroya",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Address: Ambonao, Calasiao, Pangasinan",
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.phone,
+                    color: Colors.lightBlue,
+                  ),
+                  Text(
+                    "09396330392",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+              child: Row(children: [
+                Icon(Icons.email, color: Colors.lightBlue),
+                Text(
+                  "markdaroya92@gmail.com",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ]),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Job Position",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              "Nc2 Especialist",
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                IconButton(
+                  iconSize: 40,
+                  icon: const Icon(
+                    IconData(0xe043, fontFamily: 'MaterialIcons'),
+                    color: Colors.lightBlue,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            personal(), // Use the correct widget name here
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                IconButton(
+                  iconSize: 40,
+                  icon: const Icon(
+                    IconData(0xf69b, fontFamily: 'MaterialIcons'),
+                    color: Colors.lightBlue,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            certificate(), // Use the correct widget name here
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                IconButton(
+                  iconSize: 40,
+                  icon: const Icon(
+                    IconData(0xe57f, fontFamily: 'MaterialIcons'),
+                    color: Colors.lightBlue,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            skills(), // Use the correct widget name here
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/skill.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class skills extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Skills'),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start, // Align text to the left
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
+            child: Text(
+              'Debugger',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.lightBlue,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Text(
+              'Networking',
+              style: TextStyle(
+                color: Colors.lightBlue,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Text(
+              'Good in Communication',
+              style: TextStyle(
+                color: Colors.lightBlue,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Text(
+              'Working Hard',
+              style: TextStyle(
+                color: Colors.lightBlue,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Text(
+              'Work Under Pressure',
+              style: TextStyle(
+                color: Colors.lightBlue,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Text(
+              'Honest',
+              style: TextStyle(
+                color: Colors.lightBlue,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Text(
+              'Courteous',
+              style: TextStyle(
+                color: Colors.lightBlue,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Text(
+              'Industrious',
+              style: TextStyle(
+                color: Colors.lightBlue,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Text(
+              'Work',
+              style: TextStyle(
+                color: Colors.lightBlue,
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
